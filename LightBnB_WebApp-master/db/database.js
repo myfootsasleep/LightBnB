928ad50da0086e2addb92a136be1e359e20792e6
myfootsasleep <richardoda@gmail.com>
2023-07-07 01:04:45 -0700

    refractored the getAllReservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -90,11 +90,11 @@ const addUser = function(user) {
       [user.name, user.email, user.password]
     )
     .then((result) => {
-      console.log('in .then of addUser', result);
+      //console.log('in .then of addUser', result);
       return result.rows[0];
     })
     .catch((error) => {
-      console.log('in .catch of addUser', error.message);
+      //console.log('in .catch of addUser', error.message);
     });
 };
 /// Reservations
@@ -105,9 +105,25 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+     `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+     FROM reservations
+     JOIN properties ON reservations.property_id = properties.id
+     JOIN property_reviews ON properties.id = property_reviews.property_id
+     WHERE reservations.guest_id = $1
+     GROUP BY properties.id, reservations.id
+     ORDER BY reservations.start_date
+     LIMIT $2;`, [guest_id,limit]
+    )
+    .then((result) => {
+      console.log("in .then of getAllReservations", result);
+      return result.rows;
+    })
+    .catch((error) => {
+      console.log("in getAllReservations", error);
+    });
 };
-
 /// Properties
 
 /**
